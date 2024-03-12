e92143b0f3a6b14afe2604c388ba2e467cb69742
Dennis <dennisb.galeano@gmail.com>
2024-03-12 17:13:19 -0500

    SC23 CL378 - Express Validator_Dispone muchos middlewares - es un paquete para hacer validaciones de express. Middleware: fn que se va a ejecutar antes que cualquier otra cosa.

--- controlers/auth.js
+++ controlers/auth.js
@@ -2,22 +2,37 @@
 //son las fn definidas en routes/auth ( la ruta y la peticion quedan en routes/auth)
 //peticiones
 const express = require('express');
+const { validationResult} = require('express-validator'); //middleware para el manejo de errores, me los muestra
 
 
-const crearUsuario = (req, res= express.response) => {  //router.post('/new') de routes/auth //si quiero crear un nuevo usuario la peticion debe ser tipo POST, va a apuntar a '/new', seria el equivalente a /api/auth/new   (localhost:5174/api/auth/new)1.app 2.el tipo de peticion que esta esperando en este caso GET y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
+const crearUsuario = (req, res = express.response) => {  //router.post('/new') de routes/auth //si quiero crear un nuevo usuario la peticion debe ser tipo POST, va a apuntar a '/new', seria el equivalente a /api/auth/new   (localhost:5174/api/auth/new)1.app 2.el tipo de peticion que esta esperando en este caso GET y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
  
   //console.log( req.body);   el body de postman para mandar los datos {el objeto con la informacion, nombre, correo y contraseña}
 
-  const  {name, email, password} = (req.body); //se extraen los datos del body, ya podemos obtener la informacion 
-  
+  const  { name, email, password } = req.body; //se extraen los datos del body, ya podemos obtener la informacion 
+ 
+  /* ESTA VALIDACION SE VA A HACER CON EL express validator, en routes/auth
   if( name.length < 3 ){  //si, el return es false no se ejecuta nada 
     return res.status(400).json({ //responde estatus uqe se reguiera, en ese es 400(bad requiest)
       ok:false,
       msg: 'El nombre debe tener minimo 3 letras'
     });
   } 
+  */
 
-  res.json({
+  //*Manejo de errores
+  const errors = validationResult( req );  //para obtener los errores, se importa el middleware de express-validator validationResult y se manda la (request),  el resultado de la validacion
+    // console.log(errors);
+    if( !errors.isEmpty() ) { //si, hay errores  .isEmpty(es un objeto del express validator) entonces return 
+      return res.status(400).json( { //se manda el estatus 400 por que es un bad request
+        ok: false, 
+        errors: errors.mapped() //asi se resializan todos los errores en un objetoo faacil de trabajar
+      })
+    }
+    //el return del if, evita que se ejecute l res.json. de la sig linea
+    //si todo sale bien, se va a mandar un Status(201): "Se graba informacion correctamente"
+  
+    res.status(201).json({
     ok:true,
     msg:'registro',
     name,
@@ -29,6 +44,15 @@ const crearUsuario = (req, res= express.response) => {  //router.post('/new') de
 
 const loginUsuario = (req, res=express.response ) => {  //router.post('/' )  de routes/auth //1.app 2.el tipo de peticion que esta esperando en este caso GET y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
   const  {email, password} = (req.body);
+//manejo de errores
+  const errors = validationResult ( req );
+  if( !errors.isEmpty() ){
+    return res.status(400).json({
+        ok: false,
+        errors: errors.mapped()
+    })
+  }
+  
 
   res.json({
     ok:true,
--- routes/auth.js
+++ routes/auth.js
@@ -14,16 +14,29 @@
 const express = require('express');
 const router = express.Router();
 
+const { check } = require('express-validator'); //check middleware encargado de validar un campo particular a la vez, se unsa en endpoints 
+
 
 //importar la fn, que se va a ejecutar en el router, esta es la referencia a la fn creada en auth/cntrolers
 const {crearUsuario, loginUsuario, revalidarToken} = require('../controlers/auth');
 
 //*Endpoints
-router.post('/new', crearUsuario);
+router.post(    //si necesito aplicar un midelware ",SEGUNDO VALOR" puedo poner la fn, pero como necesito varios va entre llaves [] "una coleccion de midlewares"
+'/new', //rutas
+[ //middlewares
+  check('name', 'el nombre es obligatorio').not().isEmpty(), //1. check:campo que queiro evaluar, 2.'mensaje de error').not().isEmpty uqe no debe estar vacio (el nombre, debe ser obligatorio), no deb estar vacio, enviar mas de un valor en check para que me muestre los errores, si necesito ver los errores ir a CONTROLLERS/AUTH "Manejo de errores"
+  check('email', 'el email es obligatorio').isEmail(), //si no se envia un email, va a generar error en la validacion
+  check('password', 'el password debe de ser de seis caracteres').isLength({ min: 6}), //se puede colocar cualquir cantiad de validaciones que se necesiten
+],
+crearUsuario); 
 
-
-router.post('/', loginUsuario );  //1.app 2.el tipo de peticion que esta esperando 'POST'y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
-
+router.post(  //1.app 2.el tipo de peticion que esta esperando 'POST'y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
+'/', //ruta
+[
+  check('email','el E-mail es obligstorio').isEmail(),
+  check('password', 'el password debe de ser de seis caracteres').isLength({ min: 6}),
+],
+loginUsuario );
 
 router.get('/renew', revalidarToken);  //1.app 2.el  peticion que esta esperando GET y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
 
