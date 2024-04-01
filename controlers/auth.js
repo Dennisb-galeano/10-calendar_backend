644089bed5e1679fe66babb58402c6a9aa8e6d85
Dennis <dennisb.galeano@gmail.com>
2024-04-01 15:55:05 -0500

    SC23 CL382 Crear Usuario en DB - Config de MONGOOSE- creacion de models para creacion del esquema de Usuario.

--- controlers/auth.js
+++ controlers/auth.js
@@ -2,24 +2,37 @@
 //son las fn definidas en routes/auth ( la ruta y la peticion quedan en routes/auth)
 //peticiones
 const express = require('express');
+const Usuario = require('../models/Usuario');
+
 const { validationResult} = require('express-validator'); //middleware para el manejo de errores, me los muestra
 
 
-const crearUsuario = (req, res = express.response) => {  //router.post('/new') de routes/auth //si quiero crear un nuevo usuario la peticion debe ser tipo POST, va a apuntar a '/new', seria el equivalente a /api/auth/new   (localhost:5174/api/auth/new)1.app 2.el tipo de peticion que esta esperando en este caso GET y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
+const crearUsuario = async(req, res = express.response) => {  //router.post('/new') de routes/auth //si quiero crear un nuevo usuario la peticion debe ser tipo POST, va a apuntar a '/new', seria el equivalente a /api/auth/new   (localhost:5174/api/auth/new)1.app 2.el tipo de peticion que esta esperando en este caso GET y el / 3,segundo argumento es un callbak "fn de flecha" este se dispara con el requiest y el otro el response
  
   //console.log( req.body);   el body de postman para mandar los datos {el objeto con la informacion, nombre, correo y contraseña}
 
-  const  { name, email, password } = req.body; //se extraen los datos del body, ya podemos obtener la informacion 
-
-  //*Manejo de errores, 
-
-    res.status(201).json({
-    ok:true,
-    msg:'registro',
-    name,
-    email,
-    password
-  })
+  // const  { name, email, password } = req.body; // este objeto ya tiene el email, emai.. se extraen los datos del body, ya podemos obtener la informacion 
+  try {
+    const usuario = new Usuario(req.body); //crear una nueva instancia de mi usuario
+  
+    //grabar en DB
+    await usuario.save();
+  
+  
+    //*Manejo de errores, 
+      res.status(201).json({
+      ok:true,
+      msg:'registro',
+    })
+    
+  } catch (error) { //try, intenta hacer todo lo anterior,, si ni puede,, hace el catch
+    console.log(error) //aca solo lo vere en el servidor, el mensaje msg: se le mostrara al cliente
+    res.status(500).json({ //staatus 500, error interno
+      ok: false,
+      msg:'Comunicate con el administrador'
+    })
+    
+  }
 }
 
 
---
+++ models/Usuario.js
@@ -0,0 +1,25 @@
+//crear un usuario en la base de datos., como quiero que luzca mi usuario.
+//se exporta para cuando queira trabajar con usuatios
+
+const { Schema, model, default: mongoose } = require('mongoose'); //importacion de mongoose. se va a extraer el Schema(la informacion que quiero dejar en la DB ) y el model
+
+
+const UsuarioSchema = Schema({  //objeto con la config que necesito
+
+  name: { // la informacion que se guarta es de tipo string, y es obligatorio
+    type: String,
+    require: true
+  },
+  email:{
+    type: String,
+    require: true,
+    unique: true //le estoy diciendo que no pueden haber correos duplicados 
+  },
+  password: {
+    type: String,
+    require: true
+  }
+
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema );  //doc mongoose de model. el model se va a llamar 'usuario', y el esuqema que va a usar sera UsuarioSchema
